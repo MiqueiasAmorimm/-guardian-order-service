d32bd2f7ec5b4b87f5de1ccf1e64c87660a9978d
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-07-22 03:06:27 -0300

    feat(order): implement CreateOrderUseCase

--- src/main/java/com/guardian/order_service/application/usecase/CreateOrderUseCase.java
+++ src/main/java/com/guardian/order_service/application/usecase/CreateOrderUseCase.java
@@ -2,6 +2,7 @@ package com.guardian.order_service.application.usecase;
 
 import com.guardian.order_service.domain.model.Order;
 import com.guardian.order_service.infrastructure.repository.OrderRepository;
+import com.guardian.order_service.web.dto.CreateOrderRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,8 @@ public class CreateOrderUseCase {
         this.orderRepository = orderRepository;
     }
 
-    public Order execute() {
-
+    public Order execute(CreateOrderRequest request)  {
+        Order order = new Order(request.getProductId(), request.getQuantity(), "PENDING");
+        return orderRepository.save(order);
     }
 }
