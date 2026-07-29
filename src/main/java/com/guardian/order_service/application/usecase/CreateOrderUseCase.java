8fb76a311c974718f8033cbca480ad61657299da
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-07-29 02:42:12 -0300

    feat(order): integrate CatalogClient into CreateOrderUseCase

--- src/main/java/com/guardian/order_service/application/usecase/CreateOrderUseCase.java
+++ src/main/java/com/guardian/order_service/application/usecase/CreateOrderUseCase.java
@@ -1,19 +1,25 @@
 package com.guardian.order_service.application.usecase;
 
 import com.guardian.order_service.domain.model.Order;
+import com.guardian.order_service.infrastructure.CatalogClient;
 import com.guardian.order_service.infrastructure.repository.OrderRepository;
 import com.guardian.order_service.web.dto.CreateOrderRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateOrderUseCase {
+    private final CatalogClient catalogClient;
     private final OrderRepository orderRepository;
 
-    public CreateOrderUseCase(OrderRepository orderRepository) {
+    public CreateOrderUseCase(CatalogClient catalogClient, OrderRepository orderRepository) {
+        this.catalogClient = catalogClient;
         this.orderRepository = orderRepository;
     }
 
     public Order execute(CreateOrderRequest request)  {
+        if (!catalogClient.productExists(request.getProductId())){
+            throw new IllegalArgumentException("Product not found: " + request.getProductId());
+        }
         Order order = new Order(request.getProductId(), request.getQuantity(), "PENDING");
         return orderRepository.save(order);
     }
