9f29f406b02a12137e49e15b4ea6eb617c8d7100
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-08-03 03:19:13 -0300

    feat(order): add validation to CreateOrderRequest

--- src/main/java/com/guardian/order_service/web/dto/CreateOrderRequest.java
+++ src/main/java/com/guardian/order_service/web/dto/CreateOrderRequest.java
@@ -1,6 +1,8 @@
 package com.guardian.order_service.web.dto;
 
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,6 +11,10 @@ import java.util.UUID;
 @Getter
 @AllArgsConstructor
 public class CreateOrderRequest {
+    @NotNull
     private UUID productId;
+
+    @NotNull
+    @Positive
     private Integer quantity;
 }
