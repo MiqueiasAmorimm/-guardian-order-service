36316a773705c0f75f273f792f6c496c4dfd91ed
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-08-03 02:48:29 -0300

    test(order): add unit test for CreateOrderUseCase success scenario

---
+++ src/test/java/com/guardian/order_service/application.usecase/CreateOrderUseCaseTest.java
@@ -0,0 +1,48 @@
+package com.guardian.order_service.application.usecase;
+
+import com.guardian.order_service.domain.model.Order;
+import com.guardian.order_service.infrastructure.CatalogClient;
+import com.guardian.order_service.infrastructure.repository.OrderRepository;
+import com.guardian.order_service.web.dto.CreateOrderRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CreateOrderUseCaseTest {
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private CatalogClient catalogClient;
+
+    @InjectMocks
+    private CreateOrderUseCase createOrderUseCase;
+
+    @Test
+    void shouldCreateOrderWhenProductExists () {
+    CreateOrderRequest request = new CreateOrderRequest(
+            UUID.randomUUID(),
+            2
+
+    );
+    when(catalogClient.productExists(any())).thenReturn(true);
+        Order savedOrder = new Order(request.getProductId(), request.getQuantity(), "PENDING");
+        when(orderRepository.save(any(Order.class))).thenReturn(savedOrder);
+
+        Order result = createOrderUseCase.execute(request);
+
+        assertNotNull(result);
+        assertEquals("PENDING", result.getStatus());
+    }
+
+}
