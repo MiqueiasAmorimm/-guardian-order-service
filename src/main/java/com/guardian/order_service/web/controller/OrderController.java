c3f876c2813c6506567546643503228deaebeb1f
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-08-06 02:57:34 -0300

    feat(order): add GET /orders/{id} endpoint

--- src/main/java/com/guardian/order_service/web/controller/OrderController.java
+++ src/main/java/com/guardian/order_service/web/controller/OrderController.java
@@ -1,24 +1,34 @@
 package com.guardian.order_service.web.controller;
 
 import com.guardian.order_service.application.usecase.CreateOrderUseCase;
+import com.guardian.order_service.application.usecase.GetOrderByIdUseCase;
 import com.guardian.order_service.domain.model.Order;
 import com.guardian.order_service.web.dto.CreateOrderRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     private final CreateOrderUseCase createOrderUseCase;
+    private final GetOrderByIdUseCase getOrderByIdUseCase;
 
-
-    public OrderController  (CreateOrderUseCase createOrderUseCase) {
+    public OrderController(CreateOrderUseCase createOrderUseCase,
+                           GetOrderByIdUseCase getOrderByIdUseCase) {
         this.createOrderUseCase = createOrderUseCase;
+        this.getOrderByIdUseCase = getOrderByIdUseCase;
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> findById(@PathVariable UUID id) {
+        Optional<Order> order = getOrderByIdUseCase.execute(id);
+        return order.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
@@ -26,4 +36,5 @@ public class OrderController {
         Order order = createOrderUseCase.execute(request);
         return ResponseEntity.status(201).body(order);
     }
+
 }
