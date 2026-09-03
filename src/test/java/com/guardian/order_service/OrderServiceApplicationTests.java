8c475303023c5964fde8242958a732ca3179a999
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-09-02 23:18:16 -0300

    test(order): use test profile in OrderServiceApplicationTests

--- src/test/java/com/guardian/order_service/OrderServiceApplicationTests.java
+++ src/test/java/com/guardian/order_service/OrderServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.guardian.order_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class OrderServiceApplicationTests {
 
