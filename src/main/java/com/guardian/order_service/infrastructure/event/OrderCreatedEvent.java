df57c8757d5931d067ab88a4a347b3d1aaac79cb
MiqueiasAmorimm <miqueias202201@hotmail.com>
2026-09-18 16:51:51 -0300

    wip: add OrderCreatedEvent skeleton (missing getters)

---
+++ src/main/java/com/guardian/order_service/infrastructure/event/OrderCreatedEvent.java
@@ -0,0 +1,19 @@
+package com.guardian.order_service.infrastructure.event;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public class OrderCreatedEvent {
+    private UUID orderId;
+    private BigDecimal amount;
+    private String currency;
+    private Instant createdAt;
+
+    public OrderCreatedEvent (UUID orderId, BigDecimal amount, String currency, Instant createdAt) {
+    this.orderId = orderId;
+    this.amount = amount;
+    this.currency = currency;
+    this.createdAt= createdAt;
+    }
+}
